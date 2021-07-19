b895a71c7ea07f8f0481e5c12f2b66fc0b6924b4
Evandro C <evandrocunhajunior@gmail.com>
2021-07-19 00:51:34 -0300

    att index and js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const bt_red = document.getElementById('bt-red')
 const bt_yellow = document.getElementById('bt-yellow')
 const bt_green = document.getElementById('bt-green')
+const bt_off = document.getElementById('bt-off')
 const trImg = document.getElementById('trLight')
 
 
@@ -12,10 +13,20 @@ function onYellow(){
     trImg.src = './img/trafficyellow.png'
 }
 
-function onGreen(){
+const onGreen = () =>{
     trImg.setAttribute('src', './img/trafficGreen.png')
+}
+
+function auto(){
+    setInterval(1, )
+}
+
+function off(event){
+    console.log(event)
+    trImg.src = './img/trafficLight.png'
 }
 
 bt_red.addEventListener('click', onRed)
 bt_yellow.addEventListener('click', onYellow)
 bt_green.addEventListener('click', onGreen)
+bt_off.addEventListener('click', off)
